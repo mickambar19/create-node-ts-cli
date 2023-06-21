e7ab6ac1da17987c28487555ee2fab5f98e6929d
Alexis Miguel Jimenez Olivares <alexis.m.jimenez.o@gmail.com>
2023-06-22 00:32:09 +0200

    feat: add inquirer and a more realistic sample file

--- templates/sample-test/src/sample.ts
+++ templates/sample-test/src/sample.ts
@@ -1,3 +1,36 @@
+#!/usr/bin/env node
+import { input } from '@inquirer/prompts'
+import { Argument, Command } from 'commander'
+
 export const sum = (a: number, b: number) => {
   return a + b
 }
+
+const main = () => {
+  const program = new Command()
+  program
+    .command('operations', { isDefault: true })
+    .addArgument(
+      new Argument(
+        '<operationName>',
+        'Indicates the operation that should be executed'
+      )
+    )
+    .action(async (operationName) => {
+      const operations: Record<string, (a: number, b: number) => number> = {
+        sum,
+      }
+      const a = +(await input({ message: 'Argument A:' })) || 0
+      const b = +(await input({ message: 'Argument B:' })) || 0
+      console.log(`OperationName ${operationName}, using a: ${a}, b:${b}`)
+      console.log(`${
+        operations[operationName]
+          ? `Result: ${operations[operationName](a, b)}`
+          : 'Operation not implemented yet'
+      }
+      `)
+    })
+  program.parse()
+}
+
+main()
