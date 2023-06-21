fbbaa84f8c29a404e94d5f968df29b778206d25a
Alexis Miguel Jimenez Olivares <alexis.m.jimenez.o@gmail.com>
2023-06-21 23:52:17 +0200

    feat: create sample files according to a configuration file

---
+++ src/constants.ts
@@ -0,0 +1,55 @@
+type FILE_CONFIG = {
+  variables?: string[]
+  active: boolean
+  targetFileName?: string
+}
+
+export const TEMPLATE_FILE_CONFIG: Record<string, FILE_CONFIG> = {
+  '.eslintignore': {
+    active: true,
+  },
+  '.eslintrc.json': {
+    active: true,
+  },
+  '.gitignore': {
+    active: true,
+    variables: [],
+  },
+  '.npmignore': {
+    active: true,
+    variables: [],
+  },
+  '.nvmrc': {
+    active: true,
+    variables: [],
+  },
+  '.prettierrc.json': {
+    active: true,
+    variables: [],
+  },
+  'CONTRIBUTING.md': {
+    active: true,
+    variables: [],
+  },
+  LICENSE: {
+    active: true,
+    variables: ['AUTHOR_NAME'],
+  },
+  'README.md': {
+    active: true,
+    variables: ['PACKAGE_NAME'],
+  },
+  'package.json': {
+    active: true,
+    variables: ['PACKAGE_NAME'],
+  },
+  'tsconfig-sample.json': {
+    active: true,
+    variables: [],
+    targetFileName: 'tsconfig.json',
+  },
+  'vite.config.ts': {
+    active: true,
+    variables: [],
+  },
+}
--- src/creator.ts
+++ src/creator.ts
@@ -1,33 +1,83 @@
 #!/usr/bin/env node
 import ora from 'ora'
 import { fileURLToPath } from 'url'
-import { cp } from 'node:fs/promises'
+import { readFile } from 'node:fs/promises'
 import path from 'node:path'
 import { cwd } from 'node:process'
+import { TEMPLATE_FILE_CONFIG } from './constants.js'
+import { copyFile, cp, mkdir, writeFile } from 'fs/promises'
+
+const isValidPackageName = (packageName: string): boolean => {
+  return /^([a-z]+[-_]{1}?)*[a-z]+$/.test(packageName)
+}
 
 export const creator = async (packageName: string) => {
+  if (!isValidPackageName(packageName)) {
+    console.log(
+      `
+\x1b[91m Use a valid name for your package. Visit: \x1b[0m
+\x1b[34m https://docs.npmjs.com/creating-a-package-json-file#required-name-and-version-fields\x1b[0m
+      `
+    )
+
+    return
+  }
+
   const spinner = ora('Creating basic node typescript structure').start()
-  const __dirname = fileURLToPath(import.meta.url)
-
-  const templateFolder = path.join(
-    __dirname,
-    '../../templates/typescript-all-opt-in'
-  )
-  const newPackageFolder = path.join(cwd(), packageName)
-
   setTimeout(() => {
     spinner.color = 'yellow'
     spinner.text = 'Almost done'
-  }, 2000)
-  await new Promise((resolve) => {
+  }, 400)
+  const __dirname = fileURLToPath(import.meta.url)
+  const baseFilesFolder = path.join(__dirname, '../../templates/base-files')
+  const newPackageFolder = path.join(cwd(), packageName)
+  try {
+    await mkdir(newPackageFolder)
+  } catch (error) {
+    console.log(`
+\x1b[31m An existing folder exists with the same name. \x1b[0m
+    `)
+    spinner.stop()
+    return
+  }
+  Object.entries(TEMPLATE_FILE_CONFIG).map(async ([fileName, config]) => {
+    const { variables, active, targetFileName } = config
+    const newFilePath = path.join(newPackageFolder, targetFileName || fileName)
+    const templateFilePath = path.join(baseFilesFolder, fileName)
+    if (active && (!variables || (variables && !variables.length))) {
+      await copyFile(templateFilePath, newFilePath)
+    }
+    if (active && variables && variables.length) {
+      let fileContent = await readFile(templateFilePath, { encoding: 'utf8' })
+      const variablesRegExp = new RegExp(`(${variables.join('|')})`, 'g')
+      fileContent = fileContent.replace(
+        variablesRegExp,
+        (match) => `${packageName}${match.toLowerCase()}`
+      )
+      await writeFile(newFilePath, fileContent)
+    }
+  })
+  const sampleCodeFolder = path.join(__dirname, '../../templates/sample-test')
+  await cp(sampleCodeFolder, newPackageFolder, {
+    recursive: true,
+    force: true,
+    errorOnExist: false,
+  })
+
+  await await new Promise((resolve) => {
     setTimeout(() => {
       resolve('')
     }, 1000)
   })
 
-  await cp(templateFolder, newPackageFolder, { recursive: true })
   spinner.stop()
   console.log(
-    '\x1b[32m All ready, you can start creating your CLI tool! \x1b[0m'
+    `
+\x1b[32m All ready, you can start creating your CLI tool! \x1b[0m
+  
+\x1b[32m $ cd ${packageName} && yarn install\x1b[0m
+
+\x1b[32m $ yarn run dev\x1b[0m
+  `
   )
 }
